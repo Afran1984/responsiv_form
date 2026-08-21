0a0cf60dde3c6b7788334806855d6885a9777e18
Afran1984 <afranrazzak1984@gmail.com>
2026-08-21 22:39:15 +0600

    Add layouts footer and Navbar

---
+++ resources/views/home-sections/hero.blade.php
@@ -0,0 +1,7 @@
+<section class="bg-primary text-white text-center py-5">
+    <div class="container my-5">
+        <h1 class="display-4 fw-bold">Welcome to Our Modern Website</h1>
+        <p class="lead mb-4">A simple, clean, and responsive UI built with Laravel Blade and Bootstrap 5.</p>
+        <a href="{{ route('profiles.create') }}" class="btn btn-light btn-lg text-primary fw-semibold">Registration</a>
+    </div>
+</section>
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,10 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', [ProfileController::class, 'create'])->name('profiles.create');
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/regform', [ProfileController::class, 'create'])->name('profiles.create');
 Route::post('/profiles', [ProfileController::class, 'store'])->name('profiles.store');
 Route::get('/profiles', [ProfileController::class, 'index'])->name('profiles.index');
