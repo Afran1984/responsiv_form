f690047125946fac4bbe42ab3e4247d7f7884f20
Afran1984 <afranrazzak1984@gmail.com>
2026-08-23 00:31:37 +0600

    Add some design

--- resources/views/home-sections/about.blade.php
+++ resources/views/home-sections/about.blade.php
@@ -1,1 +1,23 @@
-<h1>About</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>About Us</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="d-flex flex-column min-vh-100">
+
+    {{-- Header --}}
+    @include('layouts.navbar')
+
+    {{-- Main About Content --}}
+    <main class="container py-5 text-center">
+        <h1 class="fw-bold">About Us</h1>
+        <p class="lead text-muted">This is the about page located inside the home-sections folder.</p>
+    </main>
+
+    {{-- Footer --}}
+    @include('layouts.footer')
+
+</body>
+</html>
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -6,7 +6,7 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
-                <li class="nav-item"><a class="nav-link active" href="#">Home</a></li>
+                <li class="nav-item"><a class="nav-link active" href="{{ route('home') }}">Home</a></li>
                 <li class="nav-item"><a class="nav-link" href="{{ route('about') }}">About</a></li>
                 <li class="nav-item"><a class="nav-link" href="#">Services</a></li>
                 <li class="nav-item"><a class="nav-link" href="#">Contact</a></li>
