5b49d982226b2698aa3143c4102bdb5da969c9cb
Afran1984 <afranrazzak1984@gmail.com>
2026-08-23 00:25:07 +0600

    make a about pages

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,18 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+// use App\Http\Controllers\AboutController;
+
 
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/about', function () {
+    return view('home-sections.about');
+})->name('about');
+
+// Route::get('/about', [AboutController::class, 'index'])->name('about');
+
 Route::get('/regform', [ProfileController::class, 'create'])->name('profiles.create');
 Route::post('/profiles', [ProfileController::class, 'store'])->name('profiles.store');
 Route::get('/profiles', [ProfileController::class, 'index'])->name('profiles.index');
+
